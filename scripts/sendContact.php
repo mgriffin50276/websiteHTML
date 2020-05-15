1e26291ff8af8654b8349cf182d5910553ef6da0
mgriffin50276 <43116907+mgriffin50276@users.noreply.github.com>
2020-05-15 14:27:23 -0500

    Add files via upload

---
+++ scripts/sendContact.php
@@ -0,0 +1,52 @@
+<?php 
+// This script sends contact information for all contact forms
+
+$to = 'mgriffin50276@live.com'; //SET TO: EMAIL WHERE SHOULD THE INFO GO
+$first_name = $_POST['firstname'];
+$email = $_POST['email'];
+$contactMsg = $_POST['contactMsg'];
+
+
+//Strip Special Characters
+$first_name = strip_tags($first_name);
+$email= strip_tags($email);
+$contactMsg = strip_tags($contactMsg);
+
+//Trim Spaces
+$first_name = trim($first_name);
+$email = trim($email);
+$contactMsg = trim($contactMsg);
+
+$subject = $first_name.' wants more information';
+
+
+//Set a default answer if no on contact page
+//or if no message was entered
+if(!isset($contactMsg)){
+	$contactMsg = "No message provided.";
+}
+
+if (!isset($first_name) && !isset($email)){ 
+
+	header('location: http://fullstackbhm.com'); //PLACE REDIRECT LOCATION HERE UPON COMPLETION FAILURE.
+
+}else{
+	$headers = "From: $email";
+	
+	$message = "Hello, "."\n";
+	$message .= "$first_name has attempted to contact you via email.";
+	$message .= "Please contact them at your earliest convenience via email ";
+	$message .= "at ". $email ."\n\n";
+	$message .= $contactMsg;
+
+	mail($to, $subject, $message, $headers);
+
+	header('location: http://fullstackbhm.com'); //PLACE REDIRECT LOCATION HERE.
+}
+
+
+
+
+
+
+ ?>
